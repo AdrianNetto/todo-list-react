c548d92d5522afa6cee536521423c9b8210497d0
Adrian Netto <adrian.netto73@gmail.com>
2024-01-11 10:31:52 -0300

    first styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 //components
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import styles from './App.module.css'
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Conteúdo...</h1>
+      <main className={styles.main}>
+        <h1>conteúdo...</h1>
+      </main>
       <Footer />
     </div>
   );
